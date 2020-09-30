1ce5967b4e721f7acfaf060eec24949f0f527ad7
Jiajun Yan <yanjiajun@ifanr.com>
2020-09-30 15:00:55 +0800

    feat: add miniprogram

---
+++ miniprogram/app.js
@@ -0,0 +1,42 @@
+//app.js
+App({
+  onLaunch: function () {
+    require('./utils/sdk-wechat.3.14.5')
+    let clientID = 'a4d2d62965ddb57fa4d6'  // 应用名称: 广汽创意大赛
+    wx.BaaS.init(clientID)
+    // 展示本地存储能力
+    var logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+
+    // 登录
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      }
+    })
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              // 可以将 res 发送给后台解码出 unionId
+              this.globalData.userInfo = res.userInfo
+
+              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+              // 所以此处加入 callback 以防止这种情况
+              if (this.userInfoReadyCallback) {
+                this.userInfoReadyCallback(res)
+              }
+            }
+          })
+        }
+      }
+    })
+  },
+  globalData: {
+    userInfo: null
+  }
+})
